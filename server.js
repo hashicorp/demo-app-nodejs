a819f0377e3f3f2df0e4b833694abbc903e1c4a3
Jon Benson <bensojon@gmail.com>
2015-10-20 23:29:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello, World! This is a Node.js app version 06.");
-  response.end();
-}).listen(8888);
-
 var fs = require('fs');
 
-fs.readFile('/application/vault_data.json', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  
-  console.log(data);
+fs.readFile('/application/vault_data.json', function (err, html) {
   http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Writing out Vault secrets for the Node.js app.");
-    response.write(data);
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write("Hello, World! This is a Node.js app version 08.<br /><br />");
+
+    if (err) {
+      response.write(err);
+      return console.log(err);
+    }
+
+    response.write("Writing out Vault secrets for the Node.js app.<br /><br />");
+    response.write(html);
     response.end();
   }).listen(8888);
 });
