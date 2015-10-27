7f6a1f882bd73a61b0c6ac83017ff650e227d050
Jon Benson <bensojon@gmail.com>
2015-10-27 16:42:43 -0700

    Version 13

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 fs.readFile('/application/index.html', function (err, html) {
   http.createServer(function(request, response) {
     response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write("Hello, World! This is Node.js app version 12.<br /><br />");
+    response.write("Hello, World! This is Node.js app version 13.<br /><br />");
 
     if (err) {
       response.write(err.message);
