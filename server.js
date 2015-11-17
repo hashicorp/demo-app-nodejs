986b30d76f36b650992ced13277ef4515e667eed
Jon Benson <bensojon@gmail.com>
2015-11-17 08:40:35 -0800

    Changing to v34

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 fs.readFile('/application/index.html', function (err, html) {
   http.createServer(function(request, response) {
     response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write("Hello, World! This is Node.js app v33.");
+    response.write("Hello, World! This is Node.js app v34.");
 
     if (err) {
       response.write(err.message);
