4ce57a9bdcf23bd35f85b3efb0c61a718ec15a1e
Jon Benson <bensojon@gmail.com>
2015-11-18 10:30:13 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 fs.readFile('/application/index.html', function (err, html) {
   http.createServer(function(request, response) {
     response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write("Hello, World! This is Node.js app v34.");
+    response.write("Hello, World! This is Node.js app v35.");
 
     if (err) {
       response.write(err.message);
