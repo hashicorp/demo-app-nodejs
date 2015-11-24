fdb974c295ad4b3772c04ac0c5a66b0418050366
Jon Benson <bensojon@gmail.com>
2015-11-24 13:34:28 -0800

    Only read files if Vault directory exists

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ function handleRequest(req, res) {
   res.writeHead(200, {"Content-type":"text/html"});
   res.write("Hello, World! This is Node.js app v38.");
 
-  files = fs.readdirSync(vaultDir);
+  if (fs.existsSync(vaultDir)) {
+    files = fs.readdirSync(vaultDir);
 
-  for(var i = 0; i < files.length; i++) {
-    res.write(fs.readFileSync(vaultDir + files[i], "binary"));
+    for(var i = 0; i < files.length; i++) {
+      res.write(fs.readFileSync(vaultDir + files[i], "binary"));
+    }
   }
 
   res.end();
