b89d55a60c7875efbe3b897c2355c048e805aec5
Cameron Stokes <cameron@cameronstokes.com>
2015-12-17 14:57:10 -0800

    Update time and version to v46

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var http = require("http"),
 
 function handleRequest(req, res) {
   res.writeHead(200, {"Content-type":"text/html"});
-  res.write("Hello, World! This is Node.js app v45. Last updated at 2015-12-16T10:52.");
+  res.write("Hello, World! This is Node.js app v46. Last updated at 2015-12-17T14:56.");
 
   // Only show Vault files if the SHOW_VAULT KV is set to true in Consul
   if (fs.existsSync(vaultDir) && showVault && (showVault.toUpperCase() === "TRUE" || showVault === "1")) {
