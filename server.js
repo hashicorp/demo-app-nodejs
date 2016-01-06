05f66ea559e1acfeef670f505cc032ba7a33a86c
Jay Wallace <jaycwallace@gmail.com>
2016-01-06 09:28:48 -0800

    update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var http = require("http"),
 
 function handleRequest(req, res) {
   res.writeHead(200, {"Content-type":"text/html"});
-  res.write("Hello, World! This is Node.js app v50. Last updated at 2016-01-05T18:56:35Z.");
+  res.write("Hello, World! This is Node.js app v51. Last updated at 2016-01-05T18:56:35Z.");
 
   // Only show Vault files if the SHOW_VAULT KV is set to true in Consul
   if (fs.existsSync(vaultDir) && showVault && (showVault.toUpperCase() === "TRUE" || showVault === "1")) {
